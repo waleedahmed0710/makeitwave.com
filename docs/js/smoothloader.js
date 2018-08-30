f698b01b9adf43a4e46d37b6194fb39815edbeb6
WaveOS <hywelapbuckler@gmail.com>
2018-08-30 05:49:25 -0300

    added blog

---
+++ docs/js/smoothloader.js
@@ -0,0 +1,23 @@
+/* Simulate an ajax-loaded ad with a delayed DOM insertion */
+
+setTimeout(function() {
+  var el = $('#image1');
+  el.html('<img alt="First slide" class="first-slide" src="./img/smart_home2.png">');
+  onLoad(el);
+}, 100);
+
+setTimeout(function() {
+  var el = $('#image2');
+  el.html('	<img alt="Second slide" class="second-slide" src="./img/plugplay.png">');
+  onLoad(el);
+}, 100);
+
+setTimeout(function() {
+  var el = $('#image3');
+  el.html('<img alt="Third slide" class="third-slide" src="img/productbox.png">');
+  onLoad(el);
+}, 100);
+
+function onLoad(element) {
+  $(element).addClass('loaded');
+}

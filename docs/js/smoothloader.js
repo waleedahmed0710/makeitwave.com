b9032c6500afcb752b1ecfce98dec52a22bc2fd2
WaveOS <hywelapbuckler@gmail.com>
2018-10-16 09:48:18 -0400

    funny cartoon

--- docs/js/smoothloader.js
+++ docs/js/smoothloader.js
@@ -1,26 +1,26 @@
-/* Simulate an ajax-loaded ad with a delayed DOM insertion */
+/* Simulate an ajax-load with a delayed DOM insertion */
 
 setTimeout(function() {
   var el = $('#image1');
-  el.html('<img alt="First slide" class="first-slide" src="./img/smart_home2.png">');
+  el.html('<img alt="First slide" class="first-slide" src="./img/carousel/image1.png">');
   onLoad(el);
 }, 100);
 
 setTimeout(function() {
   var el = $('#image2');
-  el.html('	<img alt="Second slide" class="second-slide" src="./img/plugplay.png">');
+  el.html('	<img alt="Second slide" class="second-slide" src="./img/carousel/image2.png">');
   onLoad(el);
 }, 100);
 
 setTimeout(function() {
   var el = $('#image3');
-  el.html('<img alt="Third slide" class="third-slide" src="./img/productbox.png">');
+  el.html('<img alt="Third slide" class="third-slide" src="./img/carousel/image3.png">');
   onLoad(el);
 }, 100);
 
 setTimeout(function() {
   var el = $('#image4');
-  el.html('<img alt="Fouth slide" class="fourth-slide" src="./img/productbox2.png">');
+  el.html('<img alt="Fouth slide" class="fourth-slide" src="./img/carousel/image4.png">');
   onLoad(el);
 }, 100);
 

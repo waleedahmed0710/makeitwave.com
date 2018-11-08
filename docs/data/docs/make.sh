83d8f9606a318f75d7b362f36e1372b3d4c4d10f
WaveOS <hywelapbuckler@gmail.com>
2018-11-08 15:55:10 -0400

    trying amp - apparently makes the website 3 times faster, will see
    also rebuilt docs, before this goes live. let's see

--- docs/data/docs/make.sh
+++ docs/data/docs/make.sh
@@ -2,6 +2,7 @@
 #
 unset CDPATH
 
+printf "\n\n${RED}Wave®${NC} - Rebuilding Docs for products/homehub\n\n"
 cp -r products/shared-rst/* products/homehub/source
 cd products/homehub
 make clean
@@ -9,6 +10,7 @@ make html
 cd ..
 cd ..
 
+printf "\n\n${RED}Wave®${NC} - Rebuilding Docs for products/energymonitor\n\n"
 cp -r products/shared-rst/* products/energymonitor/source
 cd products/energymonitor
 make clean
@@ -16,6 +18,7 @@ make html
 cd ..
 cd ..
 
+printf "\n\n${RED}Wave®${NC} - Rebuilding Docs for products/vehicletracker\n\n"
 cp -r products/shared-rst/* products/vehicletracker/source
 cd products/vehicletracker
 make clean
@@ -23,6 +26,7 @@ make html
 cd ..
 cd ..
 
+printf "\n\n${RED}Wave®${NC} - Rebuilding Docs for products/multiroom\n\n"
 cp -r products/shared-rst/* products/multiroom/source
 cd products/multiroom
 make clean
@@ -30,6 +34,7 @@ make html
 cd ..
 cd ..
 
+printf "\n\n${RED}Wave®${NC} - Rebuilding Docs for products/signalbooster\n\n"
 cp -r products/shared-rst/* products/signalbooster/source
 cd products/signalbooster
 make clean
@@ -37,6 +42,7 @@ make html
 cd ..
 cd ..
 
+printf "\n\n${RED}Wave®${NC} - Rebuilding Docs for products/storagedevice\n\n"
 cp -r products/shared-rst/* products/storagedevice/source
 cd products/storagedevice
 make clean
@@ -44,79 +50,84 @@ make html
 cd ..
 cd ..
 
+printf "\n\n${RED}Wave®${NC} - Rebuilding Docs for products/warranty\n\n"
 cd products/warranty
 make clean
 make html
 cd ..
 cd ..
 
-
+printf "\n\n${RED}Wave®${NC} - Rebuilding Docs for software/dev_guide\n\n"
 cd software/dev_guide
 make clean
 make html
 cd ..
 cd ..
 
-
+printf "\n\n${RED}Wave®${NC} - Rebuilding Docs for software/distributors\n\n"
 cd software/distributors
 make clean
 make html
 cd ..
 cd ..
 
+printf "\n\n${RED}Wave®${NC} - Rebuilding Docs for software/wave_os\n\n"
 cd software/wave_os
 make clean
 make html
 cd ..
 cd ..
 
-
+printf "\n\n${RED}Wave®${NC} - Rebuilding Docs for organisation/google-patent-purchase\n\n"
 cd organisation/google-patent-purchase
 make clean
 make html
 cd ..
 cd ..
 
-
+printf "\n\n${RED}Wave®${NC} - Rebuilding Docs for organisation/business\n\n"
 cd organisation/business
 make clean
 make html
 cd ..
 cd ..
 
+printf "\n\n${RED}Wave®${NC} - Rebuilding Docs for organisation/finances\n\n"
 cd organisation/finances
 make clean
 make html
 cd ..
 cd ..
 
-
+printf "\n\n${RED}Wave®${NC} - Rebuilding Docs for organisation/whitepaper\n\n"
 cd organisation/whitepaper
 make clean
 make html
 cd ..
 cd ..
 
-
+printf "\n\n${RED}Wave®${NC} - Rebuilding Docs for organisation/privacy\n\n"
 cd organisation/privacy
 make clean
 make html
 cd ..
 cd ..
 
-
+printf "\n\n${RED}Wave®${NC} - Rebuilding Docs for organisation/copyright\n\n"
 cd organisation/copyright
 make clean
 make html
 cd ..
 cd ..
 
+printf "\n\n${RED}Wave®${NC} - Rebuilding Docs for organisation/terms\n\n"
 cd organisation/terms
 make clean
 make html
 cd ..
 cd ..
 
+printf "\n\n${RED}Wave®${NC} - Rebuilding Docs for organisation/website\n\n"
 cd organisation/website
 make clean
 make html
---
+++ docs/data/docs/organisation/webmaster/source/index.rst
@@ -0,0 +1,20 @@
+.. webmaster documentation master file, created by
+   sphinx-quickstart on Thu Nov  8 04:37:03 2018.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to webmaster's documentation!
+=====================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
